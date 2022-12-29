95ed4a4765f400c29783d13abdbfebfc87086c82
ExcaliburSims <ximeonzilu@gmail.com>
2022-12-29 15:54:11 +0100

    feat: add delete on redux

--- src/feature/picture.slice.js
+++ src/feature/picture.slice.js
@@ -12,8 +12,26 @@ export const picturesSlice = createSlice({
     addPicture: (state, action) => {
       state.pictures.push(action.payload);
     },
+    editPicture: (state, action) => {
+      state.pictures = state.pictures.map((pic) => {
+        if (pic.id === action.payload[1]) {
+          return {
+            ...pic,
+            artist: action.payload[0],
+          };
+        } else {
+          return pic;
+        }
+      });
+    },
+    deletePicture: (state, action) => {
+      state.pictures = state.pictures.filter(
+        (pic) => pic.id !== action.payload,
+      );
+    },
   },
 });
 
-export const { setPicturesData, addPicture } = picturesSlice.actions;
+export const { setPicturesData, addPicture, editPicture, deletePicture } =
+  picturesSlice.actions;
 export default picturesSlice.reducer;
